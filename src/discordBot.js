a6004916e9f7c5ae2e7cb8850a1b6b4141d0020f
Samuel Sau <samuel51@usf.edu>
2023-12-08 22:53:29 -0500

    found the right endpoints for websocket connections

--- src/discordBot.js
+++ src/discordBot.js
@@ -1,14 +1,139 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+import { Client, GatewayIntentBits, SlashCommandBuilder, REST, Routes } from 'discord.js';
 import {} from 'dotenv/config'
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Example in-memory structure for user inventories and bets, of course feel free to change to database if you want to permanently store the data
+const userInventories = new Map(); // Store user inventory amounts
+const userBets = new Map(); // Store user bets
 
 export function startDiscordBot() {
-	// ... rest of your Discord bot logic ...
+
+	    // Define Slash Commands
+		const commands = [
+			new SlashCommandBuilder()
+				.setName('bet')
+				.setDescription('Bet on a team')
+				.addStringOption(option =>
+					option.setName('team')
+						.setDescription('The team to bet on')
+						.setRequired(true)
+						.addChoices({ name: 'Red', value: 'red' }, { name: 'Blue', value: 'blue' }))
+				.addIntegerOption(option =>
+					option.setName('amount')
+						.setDescription('The amount to bet')
+						.setRequired(true)),
+			new SlashCommandBuilder()
+				.setName('inventory')
+				.setDescription('Check your inventory amount'),
+		].map(command => command.toJSON());
+	
+		const rest = new REST({ version: '10' }).setToken(DISCORD_BOT_TOKEN);
+	
+		(async () => {
+			try {
+				console.log('Started refreshing application (/) commands.');
+	
+				await rest.put(
+					Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+					{ body: commands },
+				);
+	
+				console.log('Successfully reloaded application (/) commands.');
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+	
+		client.on('interactionCreate', async interaction => {
+			if (!interaction.isCommand()) return;
+	
+			const { commandName } = interaction;
+	
+			if (commandName === 'bet') {
+				await betMatch(interaction);
+			} else if (commandName === 'inventory') {
+				await checkInventoryAmount(interaction);
+			}
+		});
+
 
 	client.on('ready', () => {
         console.log(`Logged in as ${client.user.tag}!`);
       });
 
 	client.login(DISCORD_BOT_TOKEN);
+}
+
+// bot will notify the users in that channel based on the functions bellow
+
+async function announceBetStart() {
+	const channelId = "YOUR_CHANNEL_ID";
+	const channel = client.channels.cache.get(channelId);
+	if (channel) {
+		channel.send('Betting for the next match has started! Use /bet to place your bets.');
+	}
+}
+
+async function betMatch(interaction) {
+    const team = interaction.options.getString('team');
+    const amount = interaction.options.getInteger('amount');
+    const userId = interaction.user.id;
+
+    if (!userInventories.has(userId) || userInventories.get(userId) < amount) {
+        await interaction.reply({ content: 'Insufficient credit to place this bet.', ephemeral: true });
+        return;
+    }
+
+    userBets.set(userId, { team, amount });
+    userInventories.set(userId, userInventories.get(userId) - amount);
+
+    await interaction.reply({ content: `Bet placed: ${amount} points on ${team} team.`, ephemeral: true });
+}
+
+
+async function checkInventoryAmount(interaction) {
+    const userId = interaction.user.id;
+    const inventoryAmount = userInventories.get(userId) || 0;
+
+    await interaction.reply({ content: `You have ${inventoryAmount} points in your inventory.`, ephemeral: true });
+}
+
+
+export function announceMatchResult(result) {
+    // 'result' should be either 'red' or 'blue'
+    // Announce the result in the channel
+    // This function needs to be called from leagueClient.js with the result of the match
+    const announcement = `The match has ended. ${result} team is victorious!`;
+    // Assuming you have a channel ID to send messages
+    const channelId = "YOUR_CHANNEL_ID";
+    const channel = client.channels.cache.get(channelId);
+    if (channel) {
+        channel.send(announcement);
+    }
+}
+
+export function distributePoints(result) {
+    // 'result' should be either 'red' or 'blue'
+    userBets.forEach((bet, userId) => {
+        if (bet.team === result) {
+            // User won the bet
+            const winnings = bet.amount * 2; // Example: double the bet amount
+            const currentInventory = userInventories.get(userId) || 0;
+            userInventories.set(userId, currentInventory + winnings);
+        }
+    });
+
+    // Clear all bets after distributing points
+    userBets.clear();
+
+    // Announce the distribution of points in the channel
+    const channelId = "YOUR_CHANNEL_ID";
+    const channel = client.channels.cache.get(channelId);
+    if (channel) {
+        channel.send('Points have been distributed to the winners.');
+    }
 }
--- src/leagueClient.js
+++ src/leagueClient.js
@@ -4,12 +4,15 @@ import  {
 	LeagueClient,
 }  from 'league-connect';
 
+import { announceBetStart, betMatch, announceMatchResult, distributePoints } from './discordBot.js';
+
+
 export async function startLeagueClient() {
     // ... all the logic for starting, handling, and monitoring the League client ...
     // 1. Authenticate to get credentials
 	const credentials = await authenticate({
 		awaitConnection: true,
-		pollInterval: 5000,
+		pollInterval: 5000, //ms? -> 5 seconds
 	});
 
 	// 2. Establish a WebSocket connection
@@ -19,14 +22,51 @@ export async function startLeagueClient() {
 		},
 	});
 
-	// 3. Subscribe to the endpoint
-	ws.subscribe('/lol-login/v1/account-state', (data, event) => {
-		// This is where you handle the data from the WebSocket.
-		// For instance, you can send a Discord message based on the event data.
-		console.log('Received data from WebSocket:', data);
+	//track down the gameflow phase
+	ws.subscribe('/lol-gameflow/v1/gameflow-phase', (data, event) => {
+		if (data === "GameStart") {
+			//give 5 minutes for betting to open
+			console.log("game has started when champ selected ended successfully");
+			announceBetStart();
 
-		// ... Discord bot logic to broadcast or reply to a message ...
+		}
+		else if (data === "InProgress") {
+			//we are currently in a match
+			console.log("we are currently in a match");
+			betMatch();
+		}
+		else if (data === "None") {
+			//or whatever is actualy the postscreen match
+			//then we give the rewards to whoever betted on the winner
+			console.log("this is after they have exited the post game stats screen");
+		}
+		else {
+			console.log(data);
+		}
 	});
+
+	//when the game ends, we need to give the rewards to the winners
+	ws.subscribe('/lol-end-of-game/v1/eog-stats-block', (data, event) => {
+		console.log('/lol-end-of-game/v1/eog-stats-block:', data); //this is important if we reach to the end of game stats screen
+		const blueTeam = data['teams'][0][100];
+		const redTeam = data['teams'][1][200];
+
+		if (blueTeam['isWinningTeam'] === true) {
+			//give rewards to everyone who betted on blue team
+			console.log('blue team won');
+			announceMatchResult("Blue"); // Announce match result
+            distributePoints();   // Distribute points based on the result
+
+		}
+		else {
+			//give rewards to everyone who betted on red team
+			console.log("red team won");
+			announceMatchResult("Red"); // Announce match result
+            distributePoints();   // Distribute points based on the result
+
+		}
+	});
+
 
 	// 4. Handle LeagueClient states
 	const client = new LeagueClient(credentials);
@@ -53,6 +93,131 @@ export async function startLeagueClient() {
 		process.exit(); // Exit the process
 	});
 }
+
+
+/*
+SummonerIDs:
+
+OmegaCube: VFm-fmnbpEDMV5bcUtJoFHvs7GXIbsXUje1M2wIUm-GOtR8
+Larvesta: NzA8Z4AJHmDiVjgmHvx_dABrrs1clOhkZtWdbJ5NPnr2x38
+pho boi: iHYG4rkavwCHeHgAUTi-iTOhkAM1YxdTRFslOYvzurGk2KQ
+Wild Inosuke: -aTPIgytYXVtik0_08jmNglnwrXUu6QKCtTe_4qU5wr7Mmc
+Bush Can Talk: JJlZzMHHmBqhpSTn946VFhIdK7Z5KCbnybc3zAwHH6CLxyY
+
+if /lol-gameflow/v1/gameflow-phase == "GameStart" (we are in champ select):
+	give 5 minutes for betting to open
+
+if /lol-gameflow/v1/gameflow-phase == "InProgress" (we are currently in a match):
+
+if /lol-gameflow/v1/gameflow-phase === "None": //or whatever is actualy the postscreen match
+	then we give the rewards to whoever betted on the winner
+
+if /lol-gameflow/v1/session (can also give us the phase and all other info), but this triggers at every phase:
+ {
+  gameClient: {
+    observerServerIp: 'spectator-consumer.na1.lol.pvp.net',
+    observerServerPort: 80,
+    running: true,
+    serverIp: '192.64.168.225',
+    serverPort: 5225,
+    visible: false
+  },
+  gameData: {
+    gameId: 4853671673,
+    gameName: '',
+    isCustomGame: true,
+    password: '',
+    playerChampionSelections: [ [Object] ],
+    queue: {
+      allowablePremadeSizes: [],
+      areFreeChampionsAllowed: true,
+      assetMutator: '',
+      category: 'Custom',
+      championsRequiredToPlay: 0,
+      description: '',
+      detailedDescription: '',
+      gameMode: 'CLASSIC',
+      gameTypeConfig: [Object],
+      id: -1,
+      isRanked: false,
+      isTeamBuilderManaged: false,
+      lastToggledOffTime: 0,
+      lastToggledOnTime: 0,
+      mapId: 11,
+      maximumParticipantListSize: 5,
+      minLevel: 0,
+      minimumParticipantListSize: 0,
+      name: '',
+      numPlayersPerTeam: 5,
+      queueAvailability: 'Available',
+      queueRewards: [Object],
+      removalFromGameAllowed: false,
+      removalFromGameDelayMinutes: 0,
+      shortName: '',
+      showPositionSelector: false,
+      spectatorEnabled: false,
+      type: 'PRACTICE_GAME'
+    },
+    spectatorsAllowed: false,
+    teamOne: [ [Object] ],
+    teamTwo: []
+  },
+  gameDodge: { dodgeIds: [], phase: 'None', state: 'Invalid' },
+  map: {
+    assets: {
+      'champ-select-background-sound': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Shared/sound/music-cs-blindpick-default.ogg',
+      'champ-select-flyout-background': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/champ-select-flyout-background.jpg',
+      'champ-select-planning-intro': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/champ-select-planning-intro.jpg',
+      'game-select-icon-active': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/game-select-icon-active.png',
+      'game-select-icon-active-video': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/video/game-select-icon-active.webm',
+      'game-select-icon-default': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/game-select-icon-default.png',
+      'game-select-icon-disabled': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/game-select-icon-disabled.png',
+      'game-select-icon-hover': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/game-select-icon-hover.png',
+      'game-select-icon-intro-video': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/video/game-select-icon-intro.webm',
+      'gameflow-background': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/gameflow-background.jpg',
+      'gameflow-background-dark': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/gameflow-background-dark.jpg',
+      'gameselect-button-hover-sound': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Shared/sound/sfx-gameselect-button-hover.ogg',
+      'icon-defeat': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/icon-defeat.png',  
+      'icon-defeat-v2': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/icon-defeat-v2.png',
+      'icon-defeat-video': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/video/icon-defeat.webm',
+      'icon-empty': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/icon-empty.png',    
+      'icon-hover': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/icon-hover.png',    
+      'icon-leaver': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/icon-leaver.png',  
+      'icon-leaver-v2': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/icon-leaver-v2.png',
+      'icon-loss-forgiven-v2': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/icon-loss-forgiven-v2.png',
+      'icon-v2': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/icon-v2.png',
+      'icon-victory': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/icon-victory.png',      'icon-victory-video': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/video/icon-victory.webm',
+      'map-north': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/map-north.png',      
+      'map-south': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/map-south.png',      
+      'music-inqueue-loop-sound': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/sound/music-inqueue-loop-summonersrift.ogg',
+      'parties-background': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/parties-background.jpg',
+      'postgame-ambience-loop-sound': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/sound/sfx-ambience-loop-summonersrift.ogg',
+      'ready-check-background': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/ready-check-background.png',
+      'ready-check-background-sound': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/sound/sfx-readycheck-sr-portal.ogg',
+      'sfx-ambience-pregame-loop-sound': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/sound/sfx-ambience-loop-summonersrift.ogg',
+      'social-icon-leaver': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/social-icon-leaver.png',
+      'social-icon-victory': 'lol-game-data/assets/content/src/LeagueClient/GameModeAssets/Classic_SRU/img/social-icon-victory.png'
+    },
+    categorizedContentBundles: {},
+    description: "The newest and most venerated battleground is known as Summoner's Rift. Traverse down one of three different paths in order to attack your enemy at their weakest point. Work with your allies to siege the enemy base and destroy their Nexus!",
+    gameMode: 'CLASSIC',
+    gameModeName: "Summoner's Rift",
+    gameModeShortName: "Summoner's Rift",
+    gameMutator: '',
+    id: 11,
+    isRGM: false,
+    mapStringId: 'SR',
+    name: "Summoner's Rift",
+    perPositionDisallowedSummonerSpells: {},
+    perPositionRequiredSummonerSpells: {},
+    platformId: '',
+    platformName: '',
+    properties: { suppressRunesMasteriesPerks: false }
+  },
+  phase: 'InProgress'
+}
+
+*/
 
 
 
